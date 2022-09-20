b6f1ccfb07d046360e91e24ecb64b94fb0314fb0
BT_4N2_07 <ahelik4984@bths.edu>
2022-09-20 11:43:12 -0400

    added print statement for movie

--- src/Sample.java
+++ src/Sample.java
@@ -1,5 +1,6 @@
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Aleksei Helik");
+        System.out.println("I don't have a favorite movie.");
     }
 }
