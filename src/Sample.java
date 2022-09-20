9f42ab8c9eb0afec57172d60db360587536b3f8e
alekseihelik <ahelik4984@bths.edu>
2022-09-20 18:27:48 -0400

    added favorite food

--- src/Sample.java
+++ src/Sample.java
@@ -1,6 +1,7 @@
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("Aleksei Helik");
+        System.out.println("My name is Aleksei Helik");
         System.out.println("I don't have a favorite movie.");
+        System.out.println("My favorite food is dressed herring.");
     }
 }
